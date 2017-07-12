20ed7b75d39cf0261d9684b3e529dc82312c75ac
Jose Thomas Dominic <josethomasd@gmail.com>
2017-07-12 10:10:58 +0530

    comment id

--- app.py
+++ app.py
@@ -236,6 +236,27 @@ def send_message(recipient_id, message_text):
         log(r.text)
     return "ok", 200
 
+def send_comment_message(recipient_id, message_text):
+
+    log("sending message to {recipient}: {text}".format(recipient=recipient_id, text=message_text))
+
+    params = {  
+        "access_token": os.environ["PAGE_ACCESS_TOKEN"]
+    }
+    headers = {
+        "Content-Type": "application/json"
+    }
+    data = json.dumps({
+        "message": {
+            "text": message_text
+        }
+    })
+    r = requests.post("https://graph.facebook.com/v2.8/"+recipient_id+"/private_replies", params=params, headers=headers, data=data)
+    if r.status_code != 200:
+        log(r.status_code)
+        log(r.text)
+    return "ok", 200
+
 def send_state(recipient_id):
 
     log("sending state to {recipient}: ".format(recipient=recipient_id))

903ba1b244ecba1ac8f80b0d5e2bc2a5f6824b72
Computer <Computer>
2017-08-05 04:04:51 -0400

    something

--- app.py
+++ app.py
@@ -264,7 +264,7 @@ def webhook():
                         send_state(sender_id)
                         time.sleep(10)
 
-                        message_data = "Hi "+sender_fname_stripped+", thanks for reaching out.. So a couple of months ago I went to lunch with a girl that used to work with me at a call center, and she pulled out her phone and showed me a video... Now I no longer work at the call center either :)\n\nHere it is: http://realendeavors.com/my-mentor"
+                        message_data = "Hi "+sender_fname_stripped+", thanks for reaching out.. So a couple of months ago I went to lunch with a girl that used to work with me at a call center, and she pulled out her phone and showed me this video: http://realendeavors.com/my-mentor"
                         send_message(sender_id, message_data)
 
                     else:
@@ -314,7 +314,7 @@ def webhook():
                             
                             #time.sleep(10)
 
-                            message_data = "Hi "+sender_fname+", thanks for commenting.. So a couple of months ago I went to lunch with a girl that used to work with me at a call center, and she pulled out her phone and showed me a video... Now I no longer work at the call center either :)\n\nHere it is: http://realendeavors.com/my-mentor"
+                            message_data = "Hi "+sender_fname+", thanks for commenting.. So a couple of months ago I went to lunch with a girl that used to work with me at a call center, and she pulled out her phone and showed me this video: http://realendeavors.com/my-mentor"
                             send_comment_message(comment_id, message_data)
             except:
                 pass
